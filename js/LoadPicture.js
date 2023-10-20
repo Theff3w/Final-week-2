979bc0110934bc4a85d9bf32582967eb89217c9a
Theff <135039636+Theff3w@users.noreply.github.com>
2023-10-20 17:09:34 +0200

    Vendredi 17h10 - Feature fonctionnelle

--- js/LoadPicture.js
+++ js/LoadPicture.js
@@ -1,7 +1,96 @@
-'use strict';
+"use strict";
 
 class LoadPicture {
+    constructor(
+        apiUrl,
+        displayElement = document.body,
+        photosPerPage = 10,
+        sortOrder = "ASC"
+    ) 
+    {
+        this.apiUrl = apiUrl;
+        this.displayElement = displayElement;
+        this.photosPerPage = photosPerPage;
+        this.sortOrder = sortOrder;
+        this.photos = [];
+        this.currentPage = 0;
+    }
+
+    async load() {
+        this.showLoadingIcon();
+
+        try {
+            const response = await fetch(this.apiUrl);
+            if (!response.ok) {
+                throw new Error(`Y'a un soucis chef. Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            if (this.sortOrder === "DESC") {
+                data.reverse();
+            }
+
+            this.photos = data;
+            this.display();
+            data.forEach(photo => console.log(photo.url));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+    display() {
+        this.removeLoadingIcon();
+
+        const start = this.currentPage * this.photosPerPage;
+        const end = start + this.photosPerPage;
+        const photosToDisplay = this.photos.slice(start, end);
+
+        const container = document.createElement("div");
+        container.classList.add("photo-container");
+
+        for (const photo of photosToDisplay) {
+            const img = document.createElement("img");
+            img.src = photo.url;
+            container.appendChild(img);
+        }
+
+        this.displayElement.appendChild(container);
+
+        if (end < this.photos.length) {
+            this.addLoadMoreButton();
+        }
+    }
+
+    showLoadingIcon() {
+        const loadingIcon = document.createElement("img");
+        loadingIcon.src = "./images/loader.png";
+        loadingIcon.classList.add("loading-icon");
+        this.displayElement.appendChild(loadingIcon);
+    }
+
+    removeLoadingIcon() {
+        const loadingIcon = this.displayElement.querySelector(".loading-icon");
+        if (loadingIcon) {
+            this.displayElement.removeChild(loadingIcon);
+        }
+    }
+
+    addLoadMoreButton() {
+        const loadMoreButton = document.createElement("button");
+        loadMoreButton.textContent = "T'en veux plus?";
+        loadMoreButton.addEventListener("click", () => {
+            this.currentPage++;
+            this.display();
+        });
     
+        this.displayElement.appendChild(loadMoreButton);
+    }
+
+    loadNextSet() {
+        this.currentPage++;
+        this.display();
+    }
 }
 
 export { LoadPicture };
--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-'use strict'
+"use strict";
+
 import { LoadPicture } from './LoadPicture.js';
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/photos';
+const displayElement = document.getElementById('photo-display');
+const loadMoreButton = document.getElementById('load-more-button');
+
+const loadPicture = new LoadPicture(apiUrl, displayElement, 10, 'ASC');
+
+loadPicture.load();
+
+loadMoreButton.addEventListener('click', () => {
+    loadPicture.loadNextSet();
+    
+    if (loadPicture.currentPage * loadPicture.photosPerPage >= loadPicture.photos.length) {
+        loadMoreButton.disabled = true;
+    }
+});
